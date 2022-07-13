3762fd9859e20402ecd5c99f417d20d7aebb4031
Cleber Carvalho <cleber.carvalhojob.gmail.com>
2022-07-13 20:51:41 -0300

    Continuação na Pratica

---
+++ Introdução JavaScript/Atividade.js
@@ -0,0 +1,50 @@
+
+
+function comparaNumeros(num1,num2) { 
+    if(!num1 || !num2) return 'Defina dois numeros '
+
+    const saoIguais = criaPrimeiraFrase(num1,num2);
+    const soma = criaSegundaFrase(num1,num2);
+
+
+    return `${saoIguais} ${soma}`
+
+}
+
+    // if ternario
+    //return saoIguais ? "São iguais" : "não sao iguais";
+
+function criaPrimeiraFrase(num1,num2){ 
+        let saoIguais = '';
+
+        if(num1 !== num2){ 
+            saoIguais = 'não';
+        }
+
+        return `Os numeros ${num1} e ${num2} ${saoIguais} são iguais.`
+};
+
+function criaSegundaFrase(num1,num2){ 
+    const soma = num1 + num2; 
+
+    let resultado10 = 'menor'; 
+    let resultado20 = 'menor';
+
+    const compara10 = soma > 10 ; 
+    const compara20 = soma > 20;
+
+    if (compara10) { 
+        resultado10 = 'maior'
+    }
+    if (compara20){ 
+        resultado20 = 'maior';
+    }
+
+    return `Sua soma é ${soma}, que e ${resultado10} que 10 e ${resultado20} que e 20.`
+
+}
+
+console.log(comparaNumeros(1,2));
+
+
+
---
+++ Introdução JavaScript/sitaxeOperadores.js
@@ -0,0 +1,23 @@
+
+
+
+// ** exponencial 
+
+// igual a 
+
+// === mesmo valor e mesmo tipo 
+
+// != valor diferente 
+// !== valor e tipo sao diferentes 
+
+
+/*
+
+(condition) ? expression1 : expression 
+mesma coisa que
+if (condition)
+    statement 
+else 
+statement
+
+*/
