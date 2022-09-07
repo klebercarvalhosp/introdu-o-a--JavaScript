1b54746edaf832448b5be204d2762aa0a2d5ae50
Cleber Carvalho <cleber.carvalhojob.gmail.com>
2022-09-07 13:37:47 -0300

    Continuação

---
+++ Introdução JavaScript/Intro Basica/main.js
@@ -0,0 +1,15 @@
+
+var nome = "Rafael Galleane";
+var idade = "29"; 
+var idade2 = "10"; 
+
+var frase = "Japao e melhor do mundo ";
+
+
+alert (idade + idade2); 
+
+console.log (nome); 
+console.log(idade + idade2); 
+console.log(frase.replace("Japão","Brasil"));
+
+//alert ("Meu primeiro js"  + nome + idade + "Anos ");
